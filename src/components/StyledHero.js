46f7772f308c1d5c6928678cee5defa0b76fe99a
mikergarrity <mikergarrity@gmail.com>
2020-01-29 19:01:07 -0600

    fixed single room background image

--- src/components/StyledHero.js
+++ src/components/StyledHero.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
 import defaultImg from '../images/room-1.jpeg';
 const StyledHero = styled.header`
-  min-height: 60vh;
+  min-height: 75vh;
   /* background: url(${defaultImg}); */
-  background: url(${props => (props.img ? props.img : defaultImg)});
+  background: url(${props =>
+    props.img ? props.img : defaultImg}) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
   display: flex;
+
   align-items: center;
   justify-content: center;
 `;
